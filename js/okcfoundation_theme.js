2fc3a84dc8ef82b6f98652af0f864cd60ebb7f5f
yann <yann.boisselier@gmail.com>
2014-04-17 10:34:15 +0200

    first commit

---
+++ js/okcfoundation_theme.js
@@ -0,0 +1,9 @@
+(function ($, Drupal) {
+
+  Drupal.behaviors.okcfoundation_theme = {
+    attach: function(context, settings) {
+      // Get your Yeti started.
+    }
+  };
+
+})(jQuery, Drupal);
---
+++ templates/page.tpl.php
@@ -0,0 +1,166 @@
+<!--.page -->
+<div role="document" class="page">
+
+  <!--.l-header region -->
+  <header role="banner" class="l-header">
+
+    <?php if ($top_bar): ?>
+      <!--.top-bar -->
+      <?php if ($top_bar_classes): ?>
+      <div class="<?php print $top_bar_classes; ?>">
+      <?php endif; ?>
+        <nav class="top-bar"<?php print $top_bar_options; ?>>
+          <ul class="title-area">
+            <li class="name"><h1><?php print $linked_site_name; ?></h1></li>
+            <li class="toggle-topbar menu-icon"><a href="#"><span><?php print $top_bar_menu_text; ?></span></a></li>
+          </ul>
+          <section class="top-bar-section">
+            <?php if ($top_bar_main_menu) :?>
+              <?php print $top_bar_main_menu; ?>
+            <?php endif; ?>
+            <?php if ($top_bar_secondary_menu) :?>
+              <?php print $top_bar_secondary_menu; ?>
+            <?php endif; ?>
+          </section>
+        </nav>
+      <?php if ($top_bar_classes): ?>
+      </div>
+      <?php endif; ?>
+      <!--/.top-bar -->
+    <?php endif; ?>
+
+    <!-- Title, slogan and menu -->
+    <?php if ($alt_header): ?>
+    <section class="row <?php print $alt_header_classes; ?>">
+
+      <?php if ($linked_logo): print $linked_logo; endif; ?>
+
+      <?php if ($site_name): ?>
+        <?php if ($title): ?>
+          <div id="site-name" class="element-invisible">
+            <strong>
+              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+            </strong>
+          </div>
+        <?php else: /* Use h1 when the content title is empty */ ?>
+          <h1 id="site-name">
+            <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><span><?php print $site_name; ?></span></a>
+          </h1>
+        <?php endif; ?>
+      <?php endif; ?>
+
+      <?php if ($site_slogan): ?>
+        <h2 title="<?php print $site_slogan; ?>" class="site-slogan"><?php print $site_slogan; ?></h2>
+      <?php endif; ?>
+
+      <?php if ($alt_main_menu): ?>
+        <nav id="main-menu" class="navigation" role="navigation">
+          <?php print ($alt_main_menu); ?>
+        </nav> <!-- /#main-menu -->
+      <?php endif; ?>
+
+      <?php if ($alt_secondary_menu): ?>
+        <nav id="secondary-menu" class="navigation" role="navigation">
+          <?php print $alt_secondary_menu; ?>
+        </nav> <!-- /#secondary-menu -->
+      <?php endif; ?>
+
+    </section>
+    <?php endif; ?>
+    <!-- End title, slogan and menu -->
+
+    <?php if (!empty($page['header'])): ?>
+      <!--.l-header-region -->
+      <section class="l-header-region row">
+        <div class="large-12 columns">
+          <?php print render($page['header']); ?>
+        </div>
+      </section>
+      <!--/.l-header-region -->
+    <?php endif; ?>
+
+  </header>
+  <!--/.l-header -->
+
+  <?php if (!empty($page['featured'])): ?>
+    <!--/.featured -->
+    <section class="l-featured row">
+      <div class="large-12 columns">
+        <?php print render($page['featured']); ?>
+      </div>
+    </section>
+    <!--/.l-featured -->
+  <?php endif; ?>
+
+  <?php if ($messages && !$zurb_foundation_messages_modal): ?>
+    <!--/.l-messages -->
+    <section class="l-messages row">
+      <div class="large-12 columns">
+        <?php if ($messages): print $messages; endif; ?>
+      </div>
+    </section>
+    <!--/.l-messages -->
+  <?php endif; ?>
+
+  <main role="main" class="row l-main">
+
+    <div class="<?php print $main_grid; ?> main columns">
+
+      <?php if (!empty($page['help'])): ?>
+        <div id="region-help" class="help panel callout">
+          <?php print render($page['help']); ?>
+        </div>
+      <?php endif; ?>
+
+      <a id="main-content"></a>
+
+      <?php if ($breadcrumb) print $breadcrumb; ?>
+
+      <?php if ($title && !$is_front): ?>
+        <?php print render($title_prefix); ?>
+        <h1 id="page-title" class="title"><?php print $title; ?></h1>
+        <?php print render($title_suffix); ?>
+      <?php endif; ?>
+
+      <?php if (!empty($tabs)): ?>
+        <?php print render($tabs); ?>
+        <?php if (!empty($tabs2)): print render($tabs2); endif; ?>
+      <?php endif; ?>
+
+      <?php if ($action_links): ?>
+        <ul class="action-links">
+          <?php print render($action_links); ?>
+        </ul>
+      <?php endif; ?>
+
+      <?php print render($page['content']); ?>
+    </div>
+    <!--/.main region -->
+
+    <?php if (!empty($page['sidebar_first'])): ?>
+      <aside role="complementary" class="<?php print $sidebar_first_grid; ?> sidebar-first columns sidebar row">
+        <?php print render($page['sidebar_first']); ?>
+      </aside>
+    <?php endif; ?>
+
+    <?php if (!empty($page['sidebar_second'])): ?>
+      <aside role="complementary" class="<?php print $sidebar_sec_grid; ?> sidebar-second columns sidebar row">
+        <?php print render($page['sidebar_second']); ?>
+      </aside>
+    <?php endif; ?>
+  </main>
+  <!--/.main-->
+
+  <!--.l-footer-->
+  <?php if (!empty($page['footer'])): ?>
+  <footer class="l-footer panel row" role="contentinfo">
+      <div class="footer large-12 columns">
+        <?php print render($page['footer']); ?>
+      </div>
+  </footer>
+  <?php endif; ?>
+  <!--/.footer-->
+
+  <?php if ($messages && $zurb_foundation_messages_modal): print $messages; endif; ?>
+</div>
+<!--/.page -->
