03ca4ff1727894f6e721316f22b42ed10b563577
yann <yann.boisselier@gmail.com>
2014-04-20 13:55:47 +0200

    transform page.tpl.php to a foundation grid layout

--- template.php
+++ template.php
@@ -32,13 +32,6 @@ function okcfoundation_theme_breadcrumb($variables) {
 }
 
 /**
- * Implements template_preprocess_page()
- */
-function okcfoundation_theme_preprocess_page(&$variables) {
-
-}
-
-/**
  * Local tasks
  */
 
@@ -48,6 +41,45 @@ function okcfoundation_theme_menu_local_task($variables) {
 
 function okcfoundation_theme_menu_local_tasks($variables) {
   return _invoke_theme_plugins(__FUNCTION__, $args = array('variables' => &$variables));
+}
+
+/**
+ * Implements template_preprocess_page()
+ */
+function okcfoundation_theme_preprocess_page(&$variables) {
+
+  // by default, content is 12.
+  $content_grid_classes = 'small-12 columns';
+  $sidebar_first_grid_classes = 'small-12 large-2 columns';
+  $sidebar_second_grid_classes = 'small-12 large-2 columns';
+
+  // change content width according to first and seconde sidebar, if they are not empty
+
+  // If the two sidebars are present
+  if (!empty($variables['page']['sidebar_first']) && !empty($variables['page']['sidebar_second'])) {
+    $content_grid_classes = 'small-12 large-6 columns';
+    $sidebar_first_grid_classes = 'small-12 large-3 columns';
+    $sidebar_second_grid_classes = 'small-12 large-3 columns';
+  }
+  // if there is only first sidebar
+  elseif(!empty($variables['page']['sidebar_first']) && empty($variables['page']['sidebar_second'])) {
+    $content_grid_classes = 'small-12 large-8 columns';
+    $sidebar_first_grid_classes = 'small-12 large-4 columns';
+  }
+  // if there is only second sidebar
+  elseif(empty($variables['page']['sidebar_first']) && !empty($variables['page']['sidebar_second'])) {
+    $content_grid_classes = 'small-12 large-8 columns';
+    $sidebar_second_grid_classes = 'small-12 large-4 columns';
+  }
+
+  // send our dynamic grid classe to the page.tpl.php template
+  $variables['content_grid_classes'] = $content_grid_classes;
+  $variables['sidebar_first_grid_classes'] = $sidebar_first_grid_classes;
+  $variables['sidebar_second_grid_classes'] = $sidebar_second_grid_classes;
+
+  // invoke theme plungins as usuals
+  _invoke_theme_plugins(__FUNCTION__, $args = array('head_elements' => &$variables));
+
 }
 
 /*
