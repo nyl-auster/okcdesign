30b9396e587b97a3f2ec5ee485f36ce04548c946
yann <yann.boisselier@gmail.com>
2014-04-20 21:02:36 +0200

    rename plugins

---
+++ plugins/menu_local_tasks__sub_nav.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Class foundation_menu_top_bar
+ *
+ * Theme local tasks as foundation sub nav
+ */
+
+class menu_local_tasks__sub_nav {
+
+  static function okcfoundation_theme_menu_local_task($variables) {
+    $link = $variables['element']['#link'];
+    $link_text = $link['title'];
+    if (!empty($variables['element']['#active'])) {
+      // Add text to indicate active tab for non-visual users.
+      $active = '<span class="element-invisible">' . t('(active tab)') . '</span>';
+
+      // If the link does not contain HTML already, check_plain() it now.
+      // After we set 'html'=TRUE the link will not be sanitized by l().
+      if (empty($link['localized_options']['html'])) {
+        $link['title'] = check_plain($link['title']);
+      }
+      $link['localized_options']['html'] = TRUE;
+      $link_text = t('!local-task-title!active', array('!local-task-title' => $link['title'], '!active' => $active));
+    }
+    return '<dt' . (!empty($variables['element']['#active']) ? ' class="active"' : '') . '>' . l($link_text, $link['href'], $link['localized_options']) . "</dt>\n";
+  }
+
+  static function okcfoundation_theme_menu_local_tasks($variables) {
+
+    $output = '';
+    if (!empty($variables['primary'])) {
+      $variables['primary']['#prefix'] = '<h2 class="element-invisible">' . t('Primary tabs') . '</h2>';
+      $variables['primary']['#prefix'] .= '<dl class="sub-nav">';
+      $variables['primary']['#suffix'] = '</dl>';
+      $output .= drupal_render($variables['primary']);
+    }
+    if (!empty($variables['secondary'])) {
+      $variables['secondary']['#prefix'] = '<h2 class="element-invisible">' . t('Secondary tabs') . '</h2>';
+      $variables['secondary']['#prefix'] .= '<dl class="tabs secondary">';
+      $variables['secondary']['#suffix'] = '</dl>';
+      $output .= drupal_render($variables['secondary']);
+    }
+
+    return $output;
+  }
+
+}
---
+++ plugins/status_messages__alert_box.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Class status_messages__alert_box
+ *
+ * Theme drupal status messages as foundation alert boxes
+ */
+class status_messages__alert_box {
+
+  function okcfoundation_theme_status_messages($variables) {
+    $display = $variables['display'];
+    $output = '';
+
+    $status_heading = array(
+      'status' => t('Status message'),
+      'error' => t('Error message'),
+      'warning' => t('Warning message'),
+      'info' => ('Status message'),
+    );
+
+    $foundation_css = array(
+      'error' => 'warning',
+      'status' => 'info',
+      'warning' => 'warning'
+    );
+
+    foreach (drupal_get_messages($display) as $type => $messages) {
+      $output .= "<div data-alert class=\"alert-box round {$foundation_css[$type]}\">\n";
+      if (!empty($status_heading[$type])) {
+        $output .= '<h2 class="element-invisible">' . $status_heading[$type] . "</h2>\n";
+      }
+      if (count($messages) > 1) {
+        $output .= " <ul>\n";
+        foreach ($messages as $message) {
+          $output .= '  <li>' . $message . "</li>\n";
+        }
+        $output .= " </ul>\n";
+      }
+      else {
+        $output .= $messages[0];
+      }
+      $output .= '<a href="#" class="close">&times;</a>';
+      $output .= "</div>\n";
+    }
+    return $output;
+  }
+
+}
