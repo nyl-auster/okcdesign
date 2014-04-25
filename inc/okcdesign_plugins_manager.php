26c4b2548164d88b48257439c6b7a3e08fd44784
yann <yann.boisselier@gmail.com>
2014-04-25 19:10:27 +0200

    remove dpm

--- inc/okcdesign_plugins_manager.php
+++ inc/okcdesign_plugins_manager.php
@@ -64,7 +64,6 @@ function okcdesign_plugins_dispatch($hook, &$arg1 = array(), &$arg2 = array(), &
 
   $plugins = okcdesign_get_plugins();
   $plugins_enabled = array_filter(theme_get_setting('okcdesign_plugins_enabled'));
-  dpm($plugins_enabled);
 
   // plug in only enabled plugins.
   foreach ($plugins_enabled  as $plugin_id) {
