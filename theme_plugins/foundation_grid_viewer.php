81e540a9072e00664b2d33edbdcaacdf7ccf7341
yann <yann.boisselier@gmail.com>
2014-05-05 19:39:33 +0200

    adjust z index for grid viewer

--- theme_plugins/foundation_grid_viewer.php
+++ theme_plugins/foundation_grid_viewer.php
@@ -7,7 +7,7 @@ class foundation_grid_viewer {
       '#type' => 'checkbox',
       '#title' => 'Show review grid in front of the theme',
       '#description' => 'By default, grid is displayed behind all other html elements of the site',
-      '#default_value' => theme_plugin_get_setting(__CLASS__, 'display_above_theme'),
+      '#default_value' => theme_plugin_get_setting(__CLASS__, 'display_above_theme', 0),
     );
     return $form;
   }
@@ -36,7 +36,7 @@ class foundation_grid_viewer {
 
   function theme_grid_viewer($columns) {
     $above = theme_plugin_get_setting(__CLASS__, 'display_above_theme', 0);
-    $z_index = $above ? '+999' : '-999';
+    $z_index = $above ? '0' : '-999';
     $html = array();
     $html[] = '<div style="z-index:' . $z_index . ';position:relative;opacity:0.5" class="row">';
     $html[] = '<div style="position:absolute;top:0;height:100%;width:100%">';
