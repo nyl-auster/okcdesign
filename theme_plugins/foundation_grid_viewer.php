a492153acbd4032eedc0d4bd4413ed06c42050d2
yann <yann.boisselier@gmail.com>
2014-05-05 19:35:40 +0200

    add settings to display grid viewer in front of the theme

--- theme_plugins/foundation_grid_viewer.php
+++ theme_plugins/foundation_grid_viewer.php
@@ -2,6 +2,16 @@
 
 class foundation_grid_viewer {
 
+  function settings_form() {
+    $form['display_above_theme'] = array(
+      '#type' => 'checkbox',
+      '#title' => 'Show review grid in front of the theme',
+      '#description' => 'By default, grid is displayed behind all other html elements of the site',
+      '#default_value' => theme_plugin_get_setting(__CLASS__, 'display_above_theme'),
+    );
+    return $form;
+  }
+
   function hook_preprocess_page(&$variables) {
     $settings = $this->get_foundation_settings();
     $columns = $settings['total_columns'];
@@ -25,8 +35,10 @@ class foundation_grid_viewer {
   }
 
   function theme_grid_viewer($columns) {
+    $above = theme_plugin_get_setting(__CLASS__, 'display_above_theme', 0);
+    $z_index = $above ? '+999' : '-999';
     $html = array();
-    $html[] = '<div style="position:relative;opacity:0.5" class="row">';
+    $html[] = '<div style="z-index:' . $z_index . ';position:relative;opacity:0.5" class="row">';
     $html[] = '<div style="position:absolute;top:0;height:100%;width:100%">';
     for($i =1; $i <= $columns; $i++) {
       $html[] = '<div style="" class="small-1 columns">';
